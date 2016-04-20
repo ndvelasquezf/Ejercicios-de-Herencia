ab0112cf3e7a004910412ef722015ca4cef8e2ca
ndvelasquezf <ndvelasquezf@unal.edu.co>
2016-04-20 02:09:49 -0500

    Create ServicioSoporte.java

---
+++ src/Herencia-Excepcion-Archivo/Servicio/ServicioSoporte.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servicio;
+
+import Exception.LibroException;
+import dao.Dao;
+import data.Soporte;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author fabian.giraldo
+ */
+public class ServicioSoporte {
+    private Dao dao;
+    private ArrayList<Soporte> soportes = null; //Debe ser mejorado notablemente.
+    public ServicioSoporte(){
+      this.dao = new Dao();
+     }
+    
+    public void cargarSoportes(String archivo) throws FileNotFoundException, LibroException{
+       this.soportes = this.dao.cargarSoportes(archivo);
+    }
+    
+    public ArrayList<Soporte> getSoportes(){
+      return this.soportes;
+    }
+    
+    public Soporte buscar(String titulo){
+        for(Soporte soporte:this.soportes){
+            if (soporte.getTitulo().equalsIgnoreCase(titulo)) 
+                return soporte;
+        }
+        return null;
+    }
+   
+    
+}
