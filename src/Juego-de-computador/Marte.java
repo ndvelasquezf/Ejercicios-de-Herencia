81011c1042f3816f9cbe6653bd274baeb4e0f16b
ndvelasquezf <ndvelasquezf@unal.edu.co>
2016-04-20 23:59:56 -0500

    Create Marte.java

---
+++ src/Juego-de-computador/Marte.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alien;
+
+/**
+ *
+ * @author Rodrigo Velasquez
+ */
+public class Marte extends Alien{
+
+    public Marte(int numeroOjos, double altura, String tipoAlien, boolean transf) {
+        super(numeroOjos, altura, tipoAlien, transf);
+    }
+		
+}
