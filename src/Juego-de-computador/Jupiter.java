fe6a2718eaa230eac008fa5e210a57d3b2b2d836
ndvelasquezf <ndvelasquezf@unal.edu.co>
2016-04-20 23:59:02 -0500

    Create Jupiter.java

---
+++ src/Juego-de-computador/Jupiter.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alien;
+
+/**
+ *
+ * @author Rodrigo Velasquez
+ */
+public class Jupiter extends Alien{
+
+	public Jupiter(int numeroOjos, double altura, String tipoAlien, boolean transf) {
+
+		super(numeroOjos, altura, tipoAlien, transf);
+	}
+}
